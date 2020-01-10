5eb09217fb7e93c8e6eeb8729b221f8c3e1f83d8
GiridharanRangaswami <giridharan28it@gmail.com>
2020-01-10 18:56:00 +0530

    new Code added for Login

---
+++ src/org/suvatha/Flipk.java
@@ -0,0 +1,23 @@
+package org.suvatha;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipk {
+
+	public static void main(String[] args) {
+		
+   System.setProperty("webdriver.chrome.driver","C:\\Users\\Giridharan\\eclipse-workspace\\SuvathaNehru\\Driver\\chromedriver.exe");
+  
+         WebDriver driver = new ChromeDriver();
+         
+         driver.get("https://www.facebook.com/");
+         
+        
+         
+         
+         
+		
+		
+	}
+}
